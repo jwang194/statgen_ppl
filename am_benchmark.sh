688bffaf2f4896fa8667c6db17f2e4bbbeeae8cb
Jeremy Wang jwang194@g.ucla.edu <jwang194@venu.cs.ucla.edu>
2023-10-23 16:37:36 -0700

    vary_nGPU

--- am_benchmark.sh
+++ am_benchmark.sh
@@ -1,4 +1,4 @@
-python generate_am.py ${1} ${2} ${3} ${4}
+python generate_am.py ${2} ${3} ${4} ${5}
 echo "Data generated."
 python distribute_benchmark.py 'am' ${1} ${2} ${3} ${4} ${5}
 echo "Inference completed."
